0ccc692944f22d1e0de4028921beeeef5f94d767
Shahzaib Khalid <shahzaib.khalid85@hotmail.com>
2017-09-19 15:07:01 +0500

    Add documentation to JavaScript code

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+/**
+ * Takes a keyCode and choses the related audio file to play and the key div to add transition. 
+ * After finding the related audio file, it plays the file and add the related transition-effect CSS class to key div.
+ * Later, after playing the audio, an event listener removes the transition class from key div.
+ * 
+ * @param {number} keyCode - The code of the key pressed (like 62, 72 etc)
+ */
 function playAudio(keyCode) {
     let soundToPlay = document.querySelector(`audio[data-key="${keyCode}"]`);
     let keyToAnimate = document.querySelector(`div[data-key="${keyCode}"]`);
@@ -12,11 +19,24 @@ function playAudio(keyCode) {
     });
 }
 
-function playAudioOnKeyDown(elt) {
-    playAudio(elt.keyCode);
+/**
+ * Gets called using addEventListener, whenever a key is pressed.
+ * Then, it find the keyCode (a numeric number associated with key) and calls @playAudio to play the 
+ * audio associated with that key.
+ * 
+ * @param {object} event - Key down event object
+ */
+function playAudioOnKeyDown(event) {
+    playAudio(event.keyCode);
 }
 
+/**
+ * Gets called by the HTML key div using 'onclick()' and it calls the @playAudio function to play the related audio.
+ * 
+ * @param {number} keyCode - A unique number associated with a unique key, provided by the HTML key div tag
+ */
 function playAudioOnClick(keyCode) {
     playAudio(keyCode);
 }
+
 window.addEventListener('keydown', playAudioOnKeyDown);
