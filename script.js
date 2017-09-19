ed47ebaba0af2eef343c9165f6bb6df6ffb5ae39
Shahzaib Khalid <shahzaib.khalid85@hotmail.com>
2017-09-19 13:57:29 +0500

    On click drum play working

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-function playAudioOnKeyDown(elt) {
-    let soundToPlay = document.querySelector(`audio[data-key="${elt.keyCode}"]`);
-    let keyToAnimate = document.querySelector(`div[data-key="${elt.keyCode}"]`);
+function playAudio(keyCode) {
+    let soundToPlay = document.querySelector(`audio[data-key="${keyCode}"]`);
+    let keyToAnimate = document.querySelector(`div[data-key="${keyCode}"]`);
     keyToAnimate.classList.add('playing');
     soundToPlay.currentTime = 0;
     soundToPlay.play();
@@ -10,5 +10,13 @@ function playAudioOnKeyDown(elt) {
             element.target.classList.remove('playing');
         }
     });
+}
+
+function playAudioOnKeyDown(elt) {
+    playAudio(elt.keyCode);
+}
+
+function playAudioOnClick(keyCode) {
+    playAudio(keyCode);
 }
 window.addEventListener('keydown', playAudioOnKeyDown);
